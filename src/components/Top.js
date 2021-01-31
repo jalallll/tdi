f9b092fd6091d0d7466d7b60fad93075f2e6db5c
Jalal <jalalq11@gmail.com>
2021-01-31 00:32:36 -0500

    cards

--- src/components/Top.js
+++ src/components/Top.js
@@ -8,7 +8,7 @@ function Top() {
 		<div className='top-container'>
 			<video src='/videos/video-2.mp4' autoPlay loop muted />
 			<h1>Building Global Empires</h1>
-			<p1>Let us guide your investment journey</p1>
+			<p>Let us guide your investment journey</p>
 			<div className='about-btns'>
 				<Button ckassName='btns' buttonStyle='btn--outline'>
 					Join us
---
+++ src/components/pages/home.js
@@ -0,0 +1,14 @@
+import '../../App.css';
+import Top from '../Top';
+
+import React from 'react';
+
+function home() {
+	return (
+		<>
+			<Top />
+		</>
+	);
+}
+
+export default home;

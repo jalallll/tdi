09b0b36d116428dde11e79cbfa529b844f702c02
Jalal <jalalq11@gmail.com>
2021-01-31 00:11:39 -0500

    half

---
+++ src/components/Top.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from './Button';
+import './Top.css';
+import '../App.css';
+
+function Top() {
+	return (
+		<div className='top-container'>
+			<video src='/videos/video-2.mp4' autoPlay loop muted />
+			<h1>Building Global Empires</h1>
+			<p1>Let us guide your investment journey</p1>
+			<div className='about-btns'>
+				<Button ckassName='btns' buttonStyle='btn--outline'>
+					Join us
+				</Button>
+			</div>
+			<p1></p1>
+		</div>
+	);
+}
+
+export default Top;

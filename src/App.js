9232f8b2a433292f28c055093125c327d8fdbd08
Jalal <jalalq11@gmail.com>
2021-02-12 22:00:22 -0500

    re-work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/home';
-import About from './components/pages/About';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 function App() {
 	return (
-		<>
-			<Router>
-				<Navbar />
-				<Switch>
-					<Route path='/' exact component={Home} />
-					<Route path='/about' exact component={About} />
-				</Switch>
-			</Router>
-		</>
+		<div>
+			<Navbar />
+		</div>
 	);
 }
 
